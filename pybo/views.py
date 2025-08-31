cc2feffd73acef904843d93b2f003335a580bba2
devwoo41 <devwooju@gmail.com>
2025-09-01 08:41:46 +0900

    url하드코딩+데이터저장

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 def index(request):
@@ -10,3 +11,11 @@ def detail(request,question_id):
     question=get_object_or_404(Question,pk=question_id)
     context={'question':question}
     return render(request,'pybo/question_detall.html',context)
+
+def answer_create(request,question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'),create_date=timezone.now())
+    return redirect('pybo:detail',question_id=question.id)
+# answer_create 함수의 매개변수 question_id는 url 매핑에 의해 값이 전달된다. 예를 들어, http://127.0.0.1:8000/pybo/answer/create/2
+# 페이지를 요청하면 question_id 매개변수에 2라는 값이 전달된다.
+# 그리고 답변을 등록할 때 텍스트창에 입력한 내용은 answer_create 함수의 첫번째 매개변수인 request 객체를 통해 읽을 수 있다.
